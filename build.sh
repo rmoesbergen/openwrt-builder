142ef668e35ac67c48de4fa67b41675ccd652e74
Ronald Moesbergen <github@rmoesbergen.nl>
2021-10-24 15:38:28 +0200

    feat: add smcroute (dlna)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-EXTRA_PACKAGES="luci luci-app-unbound luci-app-adblock python3-requests libustream-openssl nano avahi-daemon tcpdump kmod-jool jool-tools"
+EXTRA_PACKAGES="luci luci-app-unbound luci-app-adblock python3-requests libustream-openssl nano avahi-daemon tcpdump kmod-jool jool-tools smcroute"
 
 if [[ ! -d ./buildroot ]]; then
   mkdir ./buildroot
